d862c2019a0a7e2c80f7b559cc05d11e35decca1
svollebregt <42436341+svollebregt@users.noreply.github.com>
2020-04-19 15:34:19 +0200

    Small updates after feedback from TD-er
    See also: https://github.com/letscontrolit/ESPEasyPluginPlayground/pull/153

--- lib/Itho/IthoCC1101.h
+++ lib/Itho/IthoCC1101.h
@@ -1,6 +1,5 @@
 /*
  * Author: Klusjesman, modified bij supersjimmie for Arduino/ESP8266
- * Modified 17-2-2020 by svollebregt to enable SYNC1 byte changing
  */
 
 #ifndef __ITHOCC1101_H__
@@ -93,14 +92,14 @@ class IthoCC1101 : protected CC1101
 
 		//init
 		void init() { CC1101::init(); }											//init,reset CC1101
-		void initReceive(uint8_t remote);
+		void initReceive();
 		uint8_t getLastCounter() { return outIthoPacket.counter; }				//counter is increased before sending a command
 		void setSendTries(uint8_t sendTries) { this->sendTries = sendTries; }
 
 		//- deviceid should be a setting as well? random gen function? TODO
 
 		//receive
-		bool checkForNewPacket(uint8_t remote);												//check RX fifo for new data
+		bool checkForNewPacket();												//check RX fifo for new data
 		IthoPacket getLastPacket() { return inIthoPacket; }						//retrieve last received/parsed packet from remote
 		IthoCommand getLastCommand() { return inIthoPacket.command; }						//retrieve last received/parsed command from remote
 		uint8_t getLastInCounter() { return inIthoPacket.counter; }						//retrieve last received/parsed command from remote
@@ -119,7 +118,7 @@ class IthoCC1101 : protected CC1101
 
 		//init CC1101 for receiving
 		void initReceiveMessage1();
-		void initReceiveMessage2(IthoMessageType expectedMessageType, uint8_t remote);
+		void initReceiveMessage2(IthoMessageType expectedMessageType);
 
 		//init CC1101 for sending
 		void initSendMessage1();
