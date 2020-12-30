20d5c5a647fcfd69e3e03aeabc4ef652c706ffa6
svollebregt <42436341+svollebregt@users.noreply.github.com>
2020-12-30 11:30:02 +0100

    Updated to mega 20121227
    Plugin updated to mega-20121227, no other changes

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ A CC1101 868Mhz transmitter is needed (available on ebay or aliexpress)
 This plugin is using the library from: https://github.com/supersjimmie/IthoEcoFanRFT/tree/master/Master/Itho 
 made by 'supersjimmie' and 'klusjesman'
 
-For more info see also: https://gathering.tweakers.net/forum/list_messages/1690945 
+For more info see also: https://gathering.tweakers.net/forum/list_messages/1690945
+
+Latest release: ESP_Easy_mega_20201227_normal_ESP8266_4M1M.bin
 
 |CC11xx pin    |ESP pins|Description                                        |
 |:-------------|:-------|:--------------------------------------------------|
--- _P145_Itho.ino
+++ _P145_Itho.ino
@@ -67,23 +67,22 @@ struct PLUGIN_145_ExtraSettingsStruct
 	char ID3[24];
 } PLUGIN_145_ExtraSettings;
 
-
 IthoCC1101 PLUGIN_145_rf;
 //void PLUGIN_145_ITHOinterrupt() ICACHE_RAM_ATTR;
 //void PLUGIN_145_ITHOcheck() ICACHE_RAM_ATTR; //as it is called by the ISR it is better to load this in RAM too?
 
 // extra for interrupt handling
 bool PLUGIN_145_ITHOhasPacket = false;
-//Ticker PLUGIN_145_ITHOticker;
 int PLUGIN_145_State=1; // after startup it is assumed that the fan is running low
 int PLUGIN_145_OldState=1;
 int PLUGIN_145_Timer=0;
 int PLUGIN_145_LastIDindex = 0;
 int PLUGIN_145_OldLastIDindex = 0;
-//long PLUGIN_145_LastPublish=0; SV - not used for anything?
 int8_t Plugin_145_IRQ_pin=-1;
 bool PLUGIN_145_InitRunned = false;
 bool PLUGIN_145_Log = false;
+
+// volatile for interrupt function
 volatile bool PLUGIN_145_Int = false;
 volatile unsigned long PLUGIN_145_Int_time = 0;
 
@@ -110,7 +109,7 @@ boolean Plugin_145(byte function, struct EventStruct *event, String &string)
 		{
 			Device[++deviceCount].Number = PLUGIN_ID_145;
 			Device[deviceCount].Type = DEVICE_TYPE_SINGLE;
-			Device[deviceCount].VType = SENSOR_TYPE_TRIPLE;
+			Device[deviceCount].VType = Sensor_VType::SENSOR_TYPE_TRIPLE;
 			Device[deviceCount].Ports = 0;
 			Device[deviceCount].PullUpOption = false;
 			Device[deviceCount].InverseLogicOption = false;
@@ -244,7 +243,6 @@ boolean Plugin_145(byte function, struct EventStruct *event, String &string)
 		String tmpString = string;
 		String cmd = parseString(tmpString, 1);
 		String param1 = parseString(tmpString, 2);
-		//noInterrupts();
 			if (cmd.equalsIgnoreCase(F("STATE")))
 			{
 
@@ -374,7 +372,6 @@ boolean Plugin_145(byte function, struct EventStruct *event, String &string)
 					success = true;
 				}
 			}
-			//interrupts();
 	  break;
 	}
 
@@ -410,7 +407,6 @@ return success;
 
 void ICACHE_RAM_ATTR PLUGIN_145_ITHOinterrupt()
 {
-	//PLUGIN_145_ITHOticker.once_ms(10, PLUGIN_145_ITHOcheck);
 	PLUGIN_145_Int = true; //flag
 	PLUGIN_145_Int_time = millis(); //used to register time since interrupt, to make sure we don't read within 10 ms as the RX buffer needs some time to get ready
 }
